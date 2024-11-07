83eed50b4b6be7517afec223ac1fbd8f54d9449a
김두종 <2019102076@khu.ac.kr>
2024-11-07 19:12:02 +0900

    feat: 랜딩 페이지에서 LearnDocker 소개 내용 추가 - LandingPage 컴포넌트 생성 - tailwind.config.js에 커스텀 색상 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import LandingPage from './components/LandingPage';
 
 function App() {
     return (
         <>
             <Header />
+            <LandingPage />
         </>
     );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,14 @@ export default {
             fontFamily: {
                 pretendard: ['Pretendard', 'sans-serif'],
             },
+            colors: {
+                'Moby-Blue': '#1D63ED',
+                'Dark-Blue': '#00084D',
+                'Light-Blue': '#E5F2FC',
+                'Off-Black': '#17191E',
+                'Secondary-Green': '#58C126',
+                'Secondary-Red': '#C12626',
+            },
         },
     },
     plugins: [],
