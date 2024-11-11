243ccb8fdec088b5af54b285806a1f5cf7bb9162
김영관 <kimyogun78@naver.com>
2024-11-11 20:03:24 +0900

    feat: docker container와 image요청에 따른 응답 controller구현

--- backend/src/dockerapi/dockerapi.controller.ts
+++ backend/src/dockerapi/dockerapi.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { DockerapiService } from './dockerapi.service';
 
 @Controller('dockerapi')
-export class DockerapiController {}
+export class DockerapiController {
+    constructor(private dockerApiService: DockerapiService) {}
+    @Get('elements')
+    getUserContainersImages() {
+        // 사용자 Port를 Session테이블로 부터 가져와서 아래 함수 매개변수로 전달해야 합니다!
+        return this.dockerApiService.getUserContainerImages('2378');
+    }
+}
