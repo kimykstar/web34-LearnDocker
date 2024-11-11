9d12683d75a752aa4da7d590bf909d1095f99da5
Younggwan <kimyogun78@naver.com>
2024-11-11 21:28:17 +0900

    refactor: @nestjs/axios를 활용한 docker elements요청으로 변경

--- backend/src/dockerapi/dockerapi.service.ts
+++ backend/src/dockerapi/dockerapi.service.ts
@@ -1,11 +1,17 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
 
 @Injectable()
 export class DockerapiService {
-    async getUserContainerImages(userPort: string) {
-        const containerResponse = await axios.get(`http://127.0.0.1:${userPort}/containers/json`);
-        const imagesResponse = await axios.get(`http://127.0.0.1:${userPort}/containers/json`);
+    constructor(private readonly httpService: HttpService) {}
+
+    async getUserContainerImages(userPort: number) {
+        const containerResponse = await this.httpService.axiosRef.get(
+            `http://127.0.0.1:${userPort}/containers/json`
+        );
+        const imagesResponse = await this.httpService.axiosRef.get(
+            `http://127.0.0.1:${userPort}/containers/json`
+        );
         return { containers: [...containerResponse.data], images: [...imagesResponse.data] };
     }
 }
