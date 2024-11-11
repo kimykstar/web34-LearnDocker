b4c06f2a5476e6cc70bc18ddf53ec9914fab8f52
Younggwan <kimyogun78@naver.com>
2024-11-11 21:39:25 +0900

    refactor: userPort매개변수 타입 변경

--- backend/src/dockerapi/dockerapi.controller.ts
+++ backend/src/dockerapi/dockerapi.controller.ts
@@ -7,6 +7,6 @@ export class DockerapiController {
     @Get('elements')
     getUserContainersImages() {
         // 사용자 Port를 Session테이블로 부터 가져와서 아래 함수 매개변수로 전달해야 합니다!
-        return this.dockerApiService.getUserContainerImages('2378');
+        return this.dockerApiService.getUserContainerImages(2378);
     }
 }
