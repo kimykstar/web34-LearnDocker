51307fc8bc003adb7b1661798ac933fed739dc89
김영관 <kimyogun78@naver.com>
2024-11-11 18:21:25 +0900

    test: dockerapi를 통한 image, container목록 가져오기 test

---
+++ backend/src/dockerapi/dockerapi.service.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DockerapiService } from './dockerapi.service';
+
+describe('DockerapiService', () => {
+    let service: DockerapiService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [DockerapiService],
+        }).compile();
+
+        service = module.get<DockerapiService>(DockerapiService);
+    });
+
+    it('ExecID is responded', async () => {
+        const response = await service.getUserContainerImages('2378');
+        console.log(response);
+        expect(response).toBeDefined();
+    });
+});
