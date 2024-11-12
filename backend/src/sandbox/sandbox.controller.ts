12c41a4362cf2c0bc0420c0485b8e7f61d464484
김영관 <kimyogun78@naver.com>
2024-11-12 18:07:06 +0900

    feat: 사용자 명령어 입력을 위한 controller구현 및 명령 유효성 검증

--- backend/src/sandbox/sandbox.controller.ts
+++ backend/src/sandbox/sandbox.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { SandboxService } from './sandbox.service';
+import { CommandValidationPipe } from './pipes/command.pipe';
 
 @Controller('sandbox')
 export class SandboxController {
@@ -8,5 +9,11 @@ export class SandboxController {
     getUserContainersImages() {
         // 사용자 Port를 Session테이블로 부터 가져와서 아래 함수 매개변수로 전달해야 합니다!
         return this.dockerApiService.getUserContainerImages(2378);
+    }
+
+    @Post('command')
+    processUserCommand(@Body('command', CommandValidationPipe) command: string) {
+        console.log(command);
+        return command;
     }
 }
