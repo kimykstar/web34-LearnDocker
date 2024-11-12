c70dd10f6eb1a3719251e8b5819465ac683c598e
김두종 <2019102076@khu.ac.kr>
2024-11-12 22:23:12 +0900

    feat: 이미지 가져오기 페이지 mount시 서버에 퀴즈 데이터 요청 - useEffect() 훅 활용 - 에러 페이지 컴포넌트 생성   - 403, 404, 500 응답에 대한 에러 메시지 출력 - 응답으로 받은 문제 데이터 렌더링

--- frontend/src/components/ImagePullPage.tsx
+++ frontend/src/components/ImagePullPage.tsx
@@ -1,15 +1,53 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate, NavigateFunction } from 'react-router-dom';
+
+interface Quiz {
+    id: number;
+    title: string;
+    content: string;
+}
+
+const handleErrorResponse = (error: unknown, navigate: NavigateFunction) => {
+    if (axios.isAxiosError(error)) {
+        if (error.response) {
+            navigate(`/error/${error.response.status}`);
+        } else if (error.request) {
+            console.error('요청이 전송되었지만, 응답이 수신되지 않았습니다: ', error.request);
+        } else {
+            console.error(
+                '오류가 발생한 요청을 설정하는 동안 문제가 발생했습니다: ',
+                error.message
+            );
+        }
+    } else {
+        console.error('unknown error');
+    }
+};
+
 const ImagePullPage = () => {
+    const navigate = useNavigate();
+    const [quizData, setQuizData] = useState<Quiz | null>(null);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3000/quiz/1')
+            .then((response) => {
+                setQuizData(response.data);
+            })
+            .catch((error) => {
+                handleErrorResponse(error, navigate);
+            });
+    }, []);
+
     return (
         <div className='font-pretendard w-[calc(100vw-17rem)] p-4'>
             <h1 className='font-bold text-3xl text-Dark-Blue mb-3'>image 가져오기</h1>
             <section className='flex h-1/2'>
                 <div className='w-[33%] border rounded-lg border-gray-300 my-4 ml-4 mr-1 pl-4'>
                     <h2 className='font-semibold text-2xl text-Dark-Blue pt-4 pb-2'>문제</h2>
-                    <p className='font-medium text-lg text-gray-800'>
-                        이미지를 가져오는 docker 명령어를 입력하세요. <br />
-                        지원하는 이미지는 다음과 같습니다. <br />
-                        hello-world <br />
-                        nginx <br />
+                    <p className='font-medium text-lg text-gray-800 whitespace-pre-wrap'>
+                        {quizData?.content}
                     </p>
                 </div>
                 <div className='w-[50%] border rounded-lg border-gray-300 my-4 ml-1'></div>
