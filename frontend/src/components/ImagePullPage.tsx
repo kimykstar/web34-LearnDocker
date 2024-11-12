d928969541a92fabb3ca947e3d5fbe64e5a9eddd
김두종 <2019102076@khu.ac.kr>
2024-11-12 13:37:19 +0900

    refactor: 이미지 가져오기 페이지에서 불필요한 flex 속성 제거

--- frontend/src/components/ImagePullPage.tsx
+++ frontend/src/components/ImagePullPage.tsx
@@ -2,8 +2,8 @@ const ImagePullPage = () => {
     return (
         <div className='font-pretendard w-[calc(100vw-17rem)] p-4'>
             <h1 className='font-bold text-3xl text-Dark-Blue mb-3'>image 가져오기</h1>
-            <section className='flex flex-row h-1/2'>
-                <div className='flex flex-col w-[33%] border rounded-lg border-gray-300 my-4 ml-4 mr-1 pl-4'>
+            <section className='flex h-1/2'>
+                <div className='w-[33%] border rounded-lg border-gray-300 my-4 ml-4 mr-1 pl-4'>
                     <h2 className='font-semibold text-2xl text-Dark-Blue pt-4 pb-2'>문제</h2>
                     <p className='font-medium text-lg text-gray-800'>
                         이미지를 가져오는 docker 명령어를 입력하세요. <br />
