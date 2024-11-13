5cfe003acf600ca96fa0f7a6a7e50d23b13f3a3a
Younggwan <kimyogun78@naver.com>
2024-11-13 21:05:00 +0900

    refactor: Sandbox요청 리팩토링

--- backend/src/sandbox/sandbox.service.ts
+++ backend/src/sandbox/sandbox.service.ts
@@ -1,68 +1,33 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { parseStringToJson, filterContainerInfo, filterImageInfo } from './dataparse.utils';
+import { requestDockerCommand } from './exec.api';
 
 @Injectable()
 export class SandboxService {
     constructor(private readonly httpService: HttpService) {}
 
     async getUserContainerImages(containerId: string) {
-        const containers = await this.getContainerList(containerId);
-        const images = await this.getImageList(containerId);
-        return { containers, images };
-    }
-
-    async getContainerList(containerId: string) {
-        const exec = await this.httpService.axiosRef.post(
-            `http://127.0.0.1:2375/containers/${containerId}/exec`,
-            {
-                AttachStdin: false,
-                AttachStdout: true,
-                AttachStderr: true,
-                Tty: false,
-                Cmd: ['docker', 'ps', '-a', '--format', 'json'],
-            }
-        );
-        const containers = await this.httpService.axiosRef.post(
-            `http://127.0.0.1:2375/exec/${exec.data.Id}/start`,
-            {
-                Detach: false,
-                Tty: true,
-                ConsoleSize: [80, 64],
-            }
-        );
-        const containerDatas = containers.data;
-        return containerDatas === '""' ? [] : this.parseContainers(containerDatas);
+        const containers = await requestDockerCommand(this.httpService, containerId, [
+            'docker',
+            'ps',
+            '-a',
+            '--format',
+            'json',
+        ]);
+        const images = await requestDockerCommand(this.httpService, containerId, [
+            'docker',
+            'images',
+            '--format',
+            'json',
+        ]);
+        return { containers: this.parseContainers(containers), images: this.parseImages(images) };
     }
 
     parseContainers(containers: string | object) {
         if (typeof containers === 'object') containers = JSON.stringify(containers);
         const containerList = parseStringToJson(containers);
         return filterContainerInfo(containerList);
-    }
-
-    async getImageList(containerId: string) {
-        const exec = await this.httpService.axiosRef.post(
-            `http://127.0.0.1:2375/containers/${containerId}/exec`,
-            {
-                AttachStdin: false,
-                AttachStdout: true,
-                AttachStderr: true,
-                DetachKeys: 'ctrl-p,ctrl-q',
-                Tty: false,
-                Cmd: ['docker', 'images', '--format', 'json'],
-            }
-        );
-        const images = await this.httpService.axiosRef.post(
-            `http://127.0.0.1:2375/exec/${exec.data.Id}/start`,
-            {
-                Detach: false,
-                Tty: true,
-                ConsoleSize: [80, 64],
-            }
-        );
-        const imageDatas = images.data;
-        return imageDatas.length === 0 ? [] : this.parseImages(imageDatas);
     }
 
     parseImages(images: string | object) {
@@ -72,25 +37,6 @@ export class SandboxService {
     }
 
     async processUserCommand(command: string, containerId: string) {
-        const exec = await this.httpService.axiosRef.post(
-            `http://127.0.0.1:2375/containers/${containerId}/exec`,
-            {
-                AttachStdin: false,
-                AttachStdout: true,
-                AttachStderr: true,
-                Tty: false,
-                Cmd: command.split(' '),
-            }
-        );
-        const response = await this.httpService.axiosRef.post(
-            `http://127.0.0.1:2375/exec/${exec.data.Id}/start`,
-            {
-                Detach: false,
-                Tty: true,
-                ConsoleSize: [80, 64],
-            }
-        );
-
-        return response.data;
+        return requestDockerCommand(this.httpService, containerId, command.split(' '));
     }
 }
