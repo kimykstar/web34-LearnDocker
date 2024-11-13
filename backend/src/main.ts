13884b2a4309f7f4c6fcfd6a16f84096205a28c7
Se Hwan <realsehwan@gmail.com>
2024-11-14 03:33:55 +0900

    chore: install cookie-parser and add middleware

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { winstonConfig } from './common/logger/config';
 import { WinstonModule } from 'nest-winston';
+import cookieParser from 'cookie-parser'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: WinstonModule.createLogger(winstonConfig),
         bufferLogs: true,
     });
+    app.use(cookieParser());
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
