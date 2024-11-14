43296735436da59e9b22a22399c0bc2eb4b544c1
김영관 <kimyogun78@naver.com>
2024-11-14 11:45:54 +0900

    rename: 파일 이름 변경 및 변수 명 수정

--- backend/src/sandbox/sandbox.controller.ts
+++ backend/src/sandbox/sandbox.controller.ts
@@ -4,18 +4,18 @@ import { CommandValidationPipe } from './pipes/command.pipe';
 
 @Controller('sandbox')
 export class SandboxController {
-    constructor(private dockerApiService: SandboxService) {}
+    constructor(private sandboxService: SandboxService) {}
     @Get('elements')
     getUserContainersImages() {
         // 사용자 containerId를 Session테이블로 부터 가져와서 아래 함수 매개변수로 전달해야 합니다!
-        return this.dockerApiService.getUserContainerImages(
+        return this.sandboxService.getUserContainerImages(
             '13d33cb7b795a4d635f0b3bfd91d5809501876c874151a0fd4a32ad0d43a6da3'
         );
     }
 
     @Post('command')
     processUserCommand(@Body('command', CommandValidationPipe) command: string) {
-        return this.dockerApiService.processUserCommand(
+        return this.sandboxService.processUserCommand(
             command,
             '13d33cb7b795a4d635f0b3bfd91d5809501876c874151a0fd4a32ad0d43a6da3'
         );
