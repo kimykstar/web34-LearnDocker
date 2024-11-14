3a0d64ee4ffb1d6887c241053b4e7db6022e31d2
guu <qjwb2@naver.com>
2024-11-14 09:31:57 +0900

    config: 애니매이션 관련 tailwind 설정 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,6 +15,35 @@ export default {
                 'Secondary-Red': '#C12626',
             },
         },
+
+        keyframes: {
+            showAndHideFirst: {
+                '0%': { opacity: '0', visibility: 'hidden' },
+                '1%, 20%': { opacity: '1', visibility: 'visible' },
+                '21%, 100%': { opacity: '0', visibility: 'hidden' },
+            },
+            showAndHideSecond: {
+                '0%, 20%': { opacity: '0', visibility: 'hidden' },
+                '21%, 40%': { opacity: '1', visibility: 'visible' },
+                '41%, 100%': { opacity: '0', visibility: 'hidden' },
+            },
+            showAndHideThird: {
+                '0%, 40%': { opacity: '0', visibility: 'hidden' },
+                '41%, 60%': { opacity: '1', visibility: 'visible' },
+                '61%, 100%': { opacity: '0', visibility: 'hidden' },
+            },
+            showAndHideFourth: {
+                '0%, 60%': { opacity: '0', visibility: 'hidden' },
+                '61%, 80%': { opacity: '1', visibility: 'visible' },
+                '81%, 100%': { opacity: '0', visibility: 'hidden' },
+            },
+        },
+        animation: {
+            showAndHideFirst: 'showAndHideFirst 2s linear forwards',
+            showAndHideSecond: 'showAndHideSecond 2s linear forwards',
+            showAndHideThird: 'showAndHideThird 2s linear forwards',
+            showAndHideFourth: 'showAndHideFourth 2s linear forwards',
+        },
     },
     plugins: [],
 };
