8c8e196a468be7c8942819c15f4e6e7304c9fee0
guu <qjwb2@naver.com>
2024-11-14 09:33:53 +0900

    refactor: absoulte를 fixed로 수정
    - 화면 축소 확대함에 따라 absoulte는 레이아웃이 깨지는 현상이 있음
    - fixed로 수정

---
+++ frontend/src/components/visualization/ContainerNode.tsx
@@ -0,0 +1,39 @@
+import { ContainerNodeProps } from '../../types/visualization';
+
+const ContainerNode = ({
+    label,
+    icon: Icon,
+    gridRow,
+    gridColumn,
+    containerData,
+}: ContainerNodeProps) => {
+    return (
+        <div
+            className='border-gray-300 border-2 relative'
+            style={{
+                gridRowStart: gridRow,
+                gridColumnStart: gridColumn,
+            }}
+        >
+            <div className='fixed -translate-y-20 -translate-x-8 flex flex-col items-center gap-2'>
+                <span className='text-sm'>{label}</span>
+                <Icon size={64} />
+            </div>
+            <div className='flex flex-col overflow-auto'>
+                {containerData?.map(({ id, name, color }) => (
+                    <div
+                        className='border-4 rounded m-1 text-xs'
+                        style={{
+                            borderColor: color,
+                        }}
+                        key={id}
+                    >
+                        {name}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ContainerNode;
