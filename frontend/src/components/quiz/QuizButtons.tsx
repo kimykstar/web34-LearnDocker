c3bd298febc3a606ad701e709d73666f6156e825
guu <qjwb2@naver.com>
2024-11-24 19:24:24 +0900

    feat: 이전 문제 버튼 handler 추가
    - 이전 문제 버튼 handler를 추가했습니다.
    - 마지막 문제의 경우 9로 하면 빈 화면이 뜨는 버그가 있어서 8로 수정 했습니다.

--- frontend/src/components/quiz/QuizButtons.tsx
+++ frontend/src/components/quiz/QuizButtons.tsx
@@ -26,7 +26,25 @@ const QuizButtons = ({ quizId, answer }: QuizButtonsProps) => {
         // TODO: submitResult의 상태에 따라 모달창을 띄워줘야 한다.
         // 아래 console.log는 테스트 용도, 나중에 삭제해야 함
         // 여기서 해줄 작업은 없고, 아래 jsx를 return해줄 때 모달창 띄우는 코드를 추가해야 할 듯 합니다.
+
+        // default가 나오는 이유는 setState가 비동기적으로 실행 되기 때문입니다.
+        // useEffect를 사용하거나 바뀐값을 변수에 담고 변수값을 console.log로 보여줘도 될 것 같아요
+        // 일단 아직 결정된 사항이 없어서 주석에 적기만 하겠습니다.
         console.log(submitResult);
+    };
+
+    const handlePrevButtonClick = async () => {
+        if (quizId === 1) {
+            alert('처음 문제입니다');
+            return;
+        }
+
+        if (quizId === 4) {
+            navigate('/what-is-container-lifecycle');
+            return;
+        }
+
+        navigate(`/quiz/${quizId - 1}`);
     };
 
     const handleNextButtonClick = async () => {
@@ -36,7 +54,7 @@ const QuizButtons = ({ quizId, answer }: QuizButtonsProps) => {
             return;
         }
 
-        if (quizId > 9) {
+        if (quizId > 8) {
             alert('마지막 문제입니다');
             return;
         }
@@ -49,10 +67,12 @@ const QuizButtons = ({ quizId, answer }: QuizButtonsProps) => {
         navigate(`/quiz/${quizId + 1}`);
     };
 
-    // TODO: 이전. 다음버튼에 이벤트 추가가 필요합니다.
     return (
         <section className='w-[85%] flex justify-end'>
-            <button className='text-lg text-white rounded-lg bg-gray-300 hover:bg-gray-400 py-2 px-4 my-4 mx-1'>
+            <button
+                className='text-lg text-white rounded-lg bg-gray-300 hover:bg-gray-400 py-2 px-4 my-4 mx-1'
+                onClick={handlePrevButtonClick}
+            >
                 이전
             </button>
             <button
