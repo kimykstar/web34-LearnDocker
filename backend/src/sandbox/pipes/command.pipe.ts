e02131d1f9eef8845b0f27c1a82e470f941ca532
Se Hwan <realsehwan@gmail.com>
2024-11-28 13:20:46 +0900

    feat: add validation logic for push commands

--- backend/src/sandbox/pipes/command.pipe.ts
+++ backend/src/sandbox/pipes/command.pipe.ts
@@ -3,6 +3,7 @@ import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 const VALID_REGEX = /^docker\s(?:(?!.*\b(?:sh|bash|exec)\b|.*[;&|<>$`]))/;
 const VOLUME_MOUNT_CHECK_REGEX =
     /^docker\s+(?:(?:container\s+)?(?:create|run))\s+.*(?:-v\b|--volume\b)/;
+const PUSH_CHECK_REGEX = /^docker\s+(?:(?:image\s+)?(?:push))/;
 
 @Injectable()
 export class CommandValidationPipe implements PipeTransform {
@@ -10,7 +11,8 @@ export class CommandValidationPipe implements PipeTransform {
         value = value.trim();
         const baseValidation = value.match(VALID_REGEX);
         const volumeMountCheck = VOLUME_MOUNT_CHECK_REGEX.test(value);
-        const isValid = baseValidation && !volumeMountCheck;
+        const pushCheck = PUSH_CHECK_REGEX.test(value);
+        const isValid = baseValidation && !volumeMountCheck && !pushCheck;
 
         if (!isValid) throw new BadRequestException('해당 명령어는 사용이 불가능합니다!');
         return value;
