3b9b88da47aacc58e57a74447aed8db366fa2aae
guu <qjwb2@naver.com>
2024-11-28 20:05:22 +0900

    feat: tanstack/react-query 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,9 @@ import { useEffect, useRef } from 'react';
 import { QuizPage } from './components/quiz/QuizPage';
 import { Alert } from 'flowbite-react';
 import { useAlert } from './hooks/useAlert';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
     const { openAlert, message, showAlert } = useAlert();
@@ -25,40 +28,51 @@ const App = () => {
     }, []);
 
     return (
-        <div className='w-full overflow-x-hidden overflow-y-hidden'>
-            <Alert
-                color='failure'
-                className={`fixed left-1/2 -translate-x-1/2 top-6 z-50 transition-all duration-500 ease-in-out ${
-                    openAlert ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full'
-                }`}
-            >
-                {message}
-            </Alert>
-            <Header />
-            <div className='flex font-pretendard'>
-                <Sidebar />
-                <div className='ml-[17rem] mt-16 flex-row p-5 w-full z-0'>
-                    <Routes>
-                        <Route path='/' element={<LandingPage />} />
+        <QueryClientProvider client={queryClient}>
+            <div className='w-full overflow-x-hidden overflow-y-hidden'>
+                <Alert
+                    color='failure'
+                    className={`fixed left-1/2 -translate-x-1/2 top-6 z-50 transition-all duration-500 ease-in-out ${
+                        openAlert ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full'
+                    }`}
+                >
+                    {message}
+                </Alert>
+                <Header />
+                <div className='flex font-pretendard'>
+                    <Sidebar />
+                    <div className='ml-[17rem] mt-16 flex-row p-5 w-full z-0'>
+                        <Routes>
+                            <Route path='/' element={<LandingPage />} />
 
-                        <Route path='/what-is-docker' element={<WhatIsDockerPage />} />
-                        <Route path='/what-is-docker-image' element={<WhatIsDockerImagePage />} />
-                        <Route path='/what-is-docker-container' element={<DockerContainerPage />} />
-                        <Route
-                            path='/what-is-container-lifecycle'
-                            element={<DockerContainerLifeCyclePage />}
-                        />
-                        <Route
-                            path='/quiz/:quizId'
-                            element={
-                                <QuizPage showAlert={showAlert} eventSourceRef={eventSourceRef} />
-                            }
-                        />
-                        <Route path='/error/:id' element={<ErrorPage />} />
-                    </Routes>
+                            <Route path='/what-is-docker' element={<WhatIsDockerPage />} />
+                            <Route
+                                path='/what-is-docker-image'
+                                element={<WhatIsDockerImagePage />}
+                            />
+                            <Route
+                                path='/what-is-docker-container'
+                                element={<DockerContainerPage />}
+                            />
+                            <Route
+                                path='/what-is-container-lifecycle'
+                                element={<DockerContainerLifeCyclePage />}
+                            />
+                            <Route
+                                path='/quiz/:quizId'
+                                element={
+                                    <QuizPage
+                                        showAlert={showAlert}
+                                        eventSourceRef={eventSourceRef}
+                                    />
+                                }
+                            />
+                            <Route path='/error/:id' element={<ErrorPage />} />
+                        </Routes>
+                    </div>
                 </div>
             </div>
-        </div>
+        </QueryClientProvider>
     );
 };
 
