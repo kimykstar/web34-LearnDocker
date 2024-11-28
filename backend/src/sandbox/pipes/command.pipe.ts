6b618946d918621f7e53a8ec5bb292833167143f
Se Hwan <realsehwan@gmail.com>
2024-11-28 09:49:38 +0900

    fix: 볼륨 마운트 못하게 막기

--- backend/src/sandbox/pipes/command.pipe.ts
+++ backend/src/sandbox/pipes/command.pipe.ts
@@ -1,13 +1,18 @@
 import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 
 const VALID_REGEX = /^docker\s(?:(?!.*\b(?:sh|bash|exec)\b|.*[;&|<>$`]))/;
+const VOLUME_MOUNT_CHECK_REGEX =
+    /^docker\s+(?:(?:container\s+)?(?:create|run))\s+.*(?:-v\b|--volume\b)/;
 
 @Injectable()
 export class CommandValidationPipe implements PipeTransform {
     transform(value: string) {
         value = value.trim();
-        const validation = value.match(VALID_REGEX);
-        if (!validation) throw new BadRequestException('해당 명령어는 사용이 불가능합니다!');
+        const baseValidation = value.match(VALID_REGEX);
+        const volumeMountCheck = VOLUME_MOUNT_CHECK_REGEX.test(value);
+        const isValid = baseValidation && !volumeMountCheck;
+
+        if (!isValid) throw new BadRequestException('해당 명령어는 사용이 불가능합니다!');
         return value;
     }
 }
