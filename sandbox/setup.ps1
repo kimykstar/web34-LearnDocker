bab9c45dbc427dfda52ea396d8b5ded9d052a5b2
Se Hwan <realsehwan@gmail.com>
2024-11-28 09:14:17 +0900

    env: 퀴즈용 이미지 생성 스크립트 추가
    - hell-world, joke 이미지

--- sandbox/setup.ps1
+++ sandbox/setup.ps1
@@ -1,5 +1,20 @@
 # .\setup.ps1 으로 실행하세요
 
+# Set working directory to script location
 Set-Location $PSScriptRoot
-docker build --tag dind:latest --file ./host-container/Dockerfile ./host-container
-docker compose -f registry/docker-compose.yml up -d
+
+# Build DinD image
+docker build -t dind:latest -f ./host-container/Dockerfile ./host-container
+
+# Build quiz images
+docker build -t localhost/hello-world -f ./quiz-images/hello-world/Dockerfile ./quiz-images/hello-world
+docker build -t localhost/joke -f ./quiz-images/joke/Dockerfile ./quiz-images/joke
+
+# Start Registry container
+docker compose -f ./registry/docker-compose.yml up -d
+
+Start-Sleep -Seconds 5
+
+# Push images to Registry
+docker push localhost/hello-world
+docker push localhost/joke
