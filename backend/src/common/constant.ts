4ca3570e4ebad547787daf80c08854ef88a3f05e
Younggwan <kimyogun78@naver.com>
2024-12-02 17:15:37 +0900

    refactor: 상호작용 시간 30분으로 조정

--- backend/src/common/constant.ts
+++ backend/src/common/constant.ts
@@ -1,2 +1,2 @@
 export const SESSION_DURATION = 1000 * 60 * 60 * 4; // 4 hours
-export const NO_INTERACTION_TIME_LIMIT = 1000 * 60 * 60; // 1 hours
+export const NO_INTERACTION_TIME_LIMIT = 1000 * 60 * 30; // 30 minutes
