7f800d6632a3f8f994c2c41d0cff5c87050577aa
Kim younggwan <91407685+kimykstar@users.noreply.github.com>
2024-12-03 11:20:38 +0900

    Update backend/src/common/request/request.interceptor.ts
    Co-authored-by: Park Se Hwan <64393421+SH9480P@users.noreply.github.com>

--- backend/src/common/request/request.interceptor.ts
+++ backend/src/common/request/request.interceptor.ts
@@ -9,17 +9,16 @@ export class RequestInterceptor implements NestInterceptor {
         const request = context.switchToHttp().getRequest();
         const sessionId = request.cookies['sid'];
         if (
-            context.getClass().name === 'SandboxController' &&
-            context.getHandler().name === 'assignContainer'
+            context.getClass().name !== 'SandboxController' ||
+            context.getHandler().name !== 'assignContainer'
         ) {
-            return next.handle();
+          const sessionDatas = this.cacheService.get(sessionId) as UserSession;
+          const currentTime = new Date();
+          this.cacheService.set(sessionId, {
+              ...sessionDatas,
+              lastRequest: currentTime,
+          });
         }
-        const sessionDatas = this.cacheService.get(sessionId) as UserSession;
-        const currentTime = new Date();
-        this.cacheService.set(sessionId, {
-            ...sessionDatas,
-            lastRequest: currentTime,
-        });
         return next.handle();
     }
 }
