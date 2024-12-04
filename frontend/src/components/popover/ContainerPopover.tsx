99acc4e3ad3d479584b9a3b9ce71f9c79139b704
김영관 <kimyogun78@naver.com>
2024-12-04 17:12:29 +0900

    feat: Container, Image hover시 popover창 꾸미기
    - ImagePopover, ContainerPopover창 꾸미기

--- frontend/src/components/popover/ContainerPopover.tsx
+++ frontend/src/components/popover/ContainerPopover.tsx
@@ -1,4 +1,5 @@
 import { Container } from '../../types/visualization';
+import { Tooltip } from 'flowbite-react';
 
 type ContainerPopoverProps = {
     container: Container;
@@ -6,16 +7,41 @@ type ContainerPopoverProps = {
 
 const ContainerPopover = ({ container }: ContainerPopoverProps) => {
     const { id, name, image, status } = container;
+
     return (
-        <>
-            ID: {id}
-            <br />
-            Container Name: {name}
-            <br />
-            Container Image: {image}
-            <br />
-            Container Status: {status}
-        </>
+        <ul className='p-2 bg-gray-50 rounded-lg shadow-md border border-gray-200'>
+            <li className='mb-2'>
+                <div className='text-xs font-semibold text-gray-500 mb-1'>ID</div>
+                <Tooltip trigger='hover' content={id}>
+                    <div className='w-[200px] truncate overflow-hidden text-ellipsis whitespace-nowrap text-sm p-2 bg-white border border-gray-300 rounded-md'>
+                        {id}
+                    </div>
+                </Tooltip>
+            </li>
+            <li className='mb-2'>
+                <div className='text-xs font-semibold text-gray-500 mb-1'>Name</div>
+                <Tooltip trigger='hover' content={name}>
+                    <div className='w-[200px] truncate overflow-hidden text-ellipsis whitespace-nowrap text-sm p-2 bg-white border border-gray-300 rounded-md'>
+                        {name}
+                    </div>
+                </Tooltip>
+            </li>
+            <li className='mb-2'>
+                <div className='text-xs font-semibold text-gray-500 mb-1'>Image</div>
+                <Tooltip trigger='hover' content={image}>
+                    <div className='w-[200px] truncate overflow-hidden text-ellipsis whitespace-nowrap text-sm p-2 bg-white border border-gray-300 rounded-md'>
+                        {image}
+                    </div>
+                </Tooltip>
+            </li>
+            <li className='mb-2'>
+                <div className='text-xs font-semibold text-gray-500 mb-1'>Status</div>
+                <div className='w-[200px] truncate overflow-hidden text-ellipsis whitespace-nowrap text-sm p-2 bg-white border border-gray-300 rounded-md'>
+                    {status}
+                </div>
+            </li>
+        </ul>
     );
 };
+
 export default ContainerPopover;
--- frontend/src/components/popover/ImagePopover.tsx
+++ frontend/src/components/popover/ImagePopover.tsx
@@ -1,4 +1,5 @@
 import { Image } from '../../types/visualization';
+import { Tooltip } from 'flowbite-react';
 
 type ImagePopoverProps = {
     image: Image;
@@ -6,13 +7,27 @@ type ImagePopoverProps = {
 
 const ImagePopover = ({ image }: ImagePopoverProps) => {
     const { id, name } = image;
+    const filteredId = id.slice(7);
+    console.log(filteredId);
     return (
-        <>
-            ID: {id}
-            <br />
-            Image Name: {name}
-            <br />
-        </>
+        <ul className='p-2 bg-gray-50 rounded-lg shadow-md border border-gray-200'>
+            <li className='mb-2'>
+                <div className='text-xs font-semibold text-gray-500 mb-1'>ID</div>
+                <Tooltip trigger='hover' content={filteredId}>
+                    <div className='w-[200px] truncate overflow-hidden text-ellipsis whitespace-nowrap text-sm p-2 bg-white border border-gray-300 rounded-md'>
+                        {filteredId}
+                    </div>
+                </Tooltip>
+            </li>
+            <li className='mb-2'>
+                <div className='text-xs font-semibold text-gray-500 mb-1'>Name</div>
+                <Tooltip trigger='hover' content={name}>
+                    <div className='w-[200px] truncate overflow-hidden text-ellipsis whitespace-nowrap text-sm p-2 bg-white border border-gray-300 rounded-md'>
+                        {name}
+                    </div>
+                </Tooltip>
+            </li>
+        </ul>
     );
 };
 export default ImagePopover;
