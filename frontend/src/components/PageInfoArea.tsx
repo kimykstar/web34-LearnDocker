7842cebb966b173086b86bf3a0d4e1fa3292c941
Younggwan <kimyogun78@naver.com>
2024-12-05 01:33:58 +0900

    feat: Sidebar아직 풀 수 없는 문제인 경우 disable 후 설명
    - 학습 시작 버튼을 누르지 않은 경우 tooltip에 학습 시작 버튼을
      눌러주세요가 표시됨
    - 학습을 시작한 경우 현재 풀 수 없는 경우 tooltip에 아직은 풀 수
      없습니다 가 표시됨
    - 풀 수 없는 문제에 대해서는 disable시킴

--- frontend/src/components/PageInfoArea.tsx
+++ frontend/src/components/PageInfoArea.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import PageType from './PageType';
+import { Tooltip } from 'flowbite-react';
 
 type PageInfoAreaProps = {
     pageType: string;
@@ -7,10 +8,31 @@ type PageInfoAreaProps = {
     title: string;
 };
 const PageInfoArea = ({ pageType, path, title }: PageInfoAreaProps) => {
+    const quizNum = Number(path.slice(-1));
+    const currentQuizNum = Number(sessionStorage.getItem('quiz'));
+
+    const canResolve = () => {
+        return pageType === 'quiz' && quizNum <= currentQuizNum;
+    };
     return (
         <div className='flex'>
             <PageType pageType={pageType} />
-            <Link to={path}>{title}</Link>
+            {canResolve() ? (
+                <Link to={path}>{title}</Link>
+            ) : (
+                <Tooltip
+                    content={
+                        currentQuizNum === 0
+                            ? '학습 시작 버튼을 눌러주세요.'
+                            : '아직은 풀 수 없습니다.'
+                    }
+                    trigger='hover'
+                >
+                    <Link to={path} onClick={(e) => e.preventDefault()}>
+                        {title}
+                    </Link>
+                </Tooltip>
+            )}
         </div>
     );
 };
