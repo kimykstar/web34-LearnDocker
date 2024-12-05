006748eafcf19e80f8161c33f2d81ba157d33190
Se Hwan <realsehwan@gmail.com>
2024-12-05 12:27:31 +0900

    fix: learndocker.io/hello-world 추가

--- backend/src/quiz/quiz.service.ts
+++ backend/src/quiz/quiz.service.ts
@@ -31,7 +31,7 @@ export class QuizService {
                 content:
                     'Docker의 첫 걸음을 시작해볼까요?\n' +
                     'learndocker.io에서 제공하는 hello-world 이미지를 가져와보세요.\n\n' +
-                    '1. docker pull 명령어를 사용하여 hello-world 이미지를 다운로드하세요.\n' +
+                    '1. docker pull 명령어를 사용하여 learndocker.io/hello-world 이미지를 다운로드하세요.\n' +
                     '2. 이미지가 성공적으로 다운로드되면 자동으로 로컬 시스템에 저장됩니다.\n',
                 hint: `<ul class="list-disc list-inside">
   <li>docker pull <learndocker.io/이미지명> 형식으로 명령어를 작성하세요.</li>
